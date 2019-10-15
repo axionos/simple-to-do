b66f797819e21102b05a90f16d6af26514cf3998
axionos <axionoso@gmail.com>
2019-10-15 17:30:00 -0400

    build clearTask function

--- app.js
+++ app.js
@@ -1,30 +1,6 @@
-// loadEvents();
-// // load every event in the page
-// function loadEvents(){
-//   document.querySelector('form').addEventListener('submit',submit);
-// }
-// // submit data function
-// function submit(e){
-//   e.preventDefault();
-//   let input = document.querySelector('input');
-//   if(input.value != '')
-//     addTask(input.value);
-//   input.value = '';
-// }
-//
-// // add tasks
-// function addTask(task){
-//   let ul = document.querySelector('ul');
-//   let li = document.createElement('li');
-//   li.innerHTML = `<span class="delete">×</span><input type="checkbox"><label>${task}</label>`;
-//   ul.appendChild(li);
-//   document.querySelector('.tasksBoard').style.display = 'block';
-// }
-
-
-
 loadEvents = () => {
   document.querySelector('form').addEventListener('submit', submit)
+  document.getElementById('clear').addEventListener('click', clearTask)
 }
 
 submit = e => {
@@ -37,7 +13,7 @@ submit = e => {
 
 addTask = task => {
   let ul = document.querySelector('ul')
-  let li = document.createElement('li');
+  let li = document.createElement('li')
   li.innerHTML = `
     <span class="delete">×</span>
     <input type="checkbox">
@@ -45,6 +21,10 @@ addTask = task => {
   `
   ul.appendChild(li);
   document.querySelector('.tasksBoard').style.display = 'block'
+}
+
+clearTask = () => {
+  let ul = document.querySelector('ul').innerHTML = ''
 }
 
 loadEvents();
