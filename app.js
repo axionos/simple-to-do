d713d9b91ae2bfde4c997cbcbebf6cee7a8bc894
axionos <axionoso@gmail.com>
2019-10-23 16:16:57 -0400

    clean up

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-// const express = require('express')
-// const path = require('path')
-// const PORT = process.env.PORT || 5000
-// // comment
-//
-// express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-//   .get('/', (req, res) => res.render('../index.html'))
-//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 
 loadEvents = () => {
   document.querySelector('form').addEventListener('submit', submit)
