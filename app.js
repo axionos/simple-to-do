3c5a91fd4e5630dd90f472f45d9b83c34b5561f3
axionos <axionoso@gmail.com>
2019-10-23 15:27:27 -0400

    edit open port

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('../index.html'))
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 loadEvents = () => {
   document.querySelector('form').addEventListener('submit', submit)
   document.getElementById('clear').addEventListener('click', clearTask)
